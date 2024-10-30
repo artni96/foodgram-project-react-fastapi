f3310f972b40bca3c61c6baab7120632fd2effc1
Artem <artni96@yandex.ru>
2024-10-31 00:01:21 +0300

    unique constraint added to SubscriptionModel

--- backend/src/models/users.py
+++ backend/src/models/users.py
@@ -1,5 +1,5 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from backend.src.db import Base
@@ -16,3 +16,7 @@ class SubscriptionModel(Base):
         ForeignKey('user.id', ondelete='CASCADE'))
     subscriber_id: Mapped[int] = mapped_column(
         ForeignKey('user.id', ondelete='CASCADE'))
+
+    __table_args__ = (UniqueConstraint(
+        'author_id', 'subscriber_id', name='author cannot be subscriber'
+    ),)
