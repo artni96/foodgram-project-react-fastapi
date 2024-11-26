0a4fb67d1a24fe030193b3e1251e33a2ef5afd16
Artem <artni96@yandex.ru>
2024-11-26 22:24:41 +0300

    favorite recipes db tests added

---
+++ backend/tests/integration_tests/favorite_recipes/test_db.py
@@ -0,0 +1,25 @@
+import pytest
+
+from backend.src.schemas.recipes import FavoriteRecipeCreate
+
+
+@pytest.mark.order(9)
+async def test_favorite_recipes_cd(
+    db,
+    test_recipe
+):
+    recipe = test_recipe
+    favorite_recipe_data = FavoriteRecipeCreate(
+        recipe_id = recipe.id,
+        user_id=recipe.author.id
+    )
+    make_recipe_favorite = await db.favorite_recipes.create(
+        favorite_recipe_data
+    )
+    await db.commit()
+
+    await db.favorite_recipes.delete(recipe_id=make_recipe_favorite.id, user_id=recipe.author.id)
+    await db.commit()
+
+    await db.recipes.delete(id=recipe.id)
+    await db.commit()
--- backend/tests/integration_tests/shopping_cart/test_db.py
+++ backend/tests/integration_tests/shopping_cart/test_db.py
@@ -1,8 +1,6 @@
 import pytest
-from sqlalchemy import select
 
-from backend.src.models.recipes import ShoppingCartModel
-from backend.src.schemas.recipes import ShoppingCartRecipeCreate, RecipeCreateRequest
+from backend.src.schemas.recipes import ShoppingCartRecipeCreate
 
 
 @pytest.mark.order(9)
@@ -19,12 +17,8 @@ async def test_shopping_cart_cd(
         shopping_cart_recipe_data
     )
     await db.commit()
-    shopping_cart_list_stmt = select(ShoppingCartModel)
-    shopping_cart_list = await db.session.execute(shopping_cart_list_stmt)
-    shopping_cart_list = shopping_cart_list.scalars().all()
-    assert shopping_cart_list[-1].id == add_in_shopping_cart.id
 
-    await db.shopping_cart.delete(recipe_id=recipe.id, user_id=recipe.author.id)
+    await db.shopping_cart.delete(recipe_id=add_in_shopping_cart.id, user_id=recipe.author.id)
     await db.commit()
 
     await db.recipes.delete(id=recipe.id)
