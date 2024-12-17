6917f9de310506f3fc13ec5101199d7e6be54de0
Artem <artni96@yandex.ru>
2024-12-17 14:12:01 +0300

    time at shopping lists changed

--- backend/src/utils/pdf_shopping_list.py
+++ backend/src/utils/pdf_shopping_list.py
@@ -1,8 +1,8 @@
-import os
+from datetime import datetime
 
 import jinja2
 import pdfkit
-from datetime import datetime
+from pytz import timezone
 
 
 def create_shopping_list(data, username):
@@ -22,7 +22,7 @@ def create_shopping_list(data, username):
     output_text = template.render(context)
 
     config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
-    filename = f'{datetime.now().strftime("%d.%m.%Y_%H:%M:%S")}_{username}_shopping_list.pdf'
+    filename = f'{datetime.now(tz=timezone("Europe/Moscow")).strftime("%d.%m.%Y_%H:%M:%S")}_{username}_shopping_list.pdf'
     output_pdf = f'src/media/shopping_cart/shopping_lists/{filename}'
     pdfkit.from_string(output_text, output_pdf, configuration=config)
     return output_pdf
